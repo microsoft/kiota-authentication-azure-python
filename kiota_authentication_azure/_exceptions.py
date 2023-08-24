11871fe6609deaa42b91b60ea5e75d074476ad72
Martin Musale <martinmusale@microsoft.com>
2023-08-24 14:07:27 +0300

    Add custom exceptions
    Signed-off-by: Martin Musale <martinmusale@microsoft.com>

---
+++ kiota_authentication_azure/_exceptions.py
@@ -0,0 +1,6 @@
+class KiotaAuthenticationError(Exception):
+    """Base class for all Kiota Authentication Azure errors."""
+
+
+class HTTPError(KiotaAuthenticationError):
+    """Error raised when the scheme fails https validation."""
